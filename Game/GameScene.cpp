94b397279ae4f0d94395312192662a85634129a1
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-10-10 17:15:03 +0900

    random追加

--- Game/GameScene.cpp
+++ Game/GameScene.cpp
@@ -4,6 +4,7 @@
 #include "GlobalVariables.h"
 #include "CameraManager.h"
 #include "FPSKeeper.h"
+#include "Random.h"
 
 
 GameScene::GameScene() {}
@@ -131,7 +132,7 @@ void GameScene::Update() {
 	float rotaSpeed = 0.1f;
 	for (auto suzunneModel : suzunnes) {
 		suzunneModel->transform.rotate.x += rotaSpeed * FPSKeeper::DeltaTime();
-
+		suzunneModel->transform.translate = Random::GetVector3({ -4.0f,4.0f }, { -4.0f,4.0f }, { -4.0f,4.0f });
 		rotaSpeed += 0.05f;
 	}
 
