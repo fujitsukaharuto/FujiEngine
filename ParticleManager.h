ed9d1273f78f6a98c37d6554ac949d994f3d2ee1
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-10-14 22:23:54 +0900

    修正

--- ParticleManager.h
+++ ParticleManager.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <wrl/client.h>
 #include "Model.h"
+#include "MatrixCalculation.h"
 
+using namespace Microsoft::WRL;
 
 
 class Particle;
@@ -15,10 +18,10 @@ public:
 	struct ParticleGroup {
 		Model* model_;
 		std::list<Particle> particles_;
-
-
-
-
+		uint32_t srvIndex_;
+		ComPtr<ID3D12Resource> instancing_;
+		uint32_t insstanceCount_;
+		TransformationParticleMatrix* instancingData_;
 	};
 
 
