67127dd8c33de1e30f29b1cf77afbf12fd39d28c
kase-haruto <0te06559726481t@au.com>
2024-10-17 07:49:10 +0900

    戦闘が音符で後ろが敵の時に音符が線にたどり着いたら連結が解除されるようにしました。

--- Game/Object/ChainEnemy.cpp
+++ Game/Object/ChainEnemy.cpp
@@ -45,19 +45,48 @@ void ChainEnemy::Update(){
 	for (size_t i = 0; i < connectedEnemies_.size(); ++i){
 		if (connectedEnemies_[i]){
 
+
+			/////////////////////////////////////////////////////////////////////////////////////////
+			/*                                     連結が切れるときの処理    　                        */
+			/////////////////////////////////////////////////////////////////////////////////////////
+
+			//距離を比べてxが小さいほうに処理
+			for (size_t j = 0; j < connectedEnemies_.size(); ++j){
+				if (connectedEnemies_[j] && j != i){
+
+					// X 座標が小さい敵に特定の処理を行う
+					if (connectedEnemies_[j]->GetWorldPosition().x < connectedEnemies_[i]->GetWorldPosition().x){
+
+						//先頭の敵が音符になっていて後ろの敵が音符になっていない場合、
+						if (connectedEnemies_[j]->GetIsChangedNote() && !connectedEnemies_[i]->GetIsChangedNote()){
+
+							//先頭の敵が線にたどり着いたら連結を解除する
+							if (connectedEnemies_[j]->GetWorldPosition().x <= Field::fieldEndPosX + Field::scrollX_){
+								isChain_ = false;
+							}
+
+						}
+
+					}
+
+				}
+			}
+
 			// 音符に変わっていない場合、Field::EndPosXを超えたら削除
 			if (!connectedEnemies_[i]->GetIsChangedNote() && connectedEnemies_[i]->GetWorldPosition().x < Field::fieldEndPosX + Field::scrollX_ + connectedEnemies_[i]->GetScale().x * 0.5f){
 				CollisionManager::GetInstance()->RemoveCollider(connectedEnemies_[i].get());
 				//障害物の生成
 				pEnemyManager_->PopObstacle();
 				connectedEnemies_[i].reset();  // 安全に削除
+				isChain_ = false;
 				removeCount_++;
 
 			} else if (connectedEnemies_[i]->GetIsRemoved()){
 				// 削除フラグが立っている場合
 				CollisionManager::GetInstance()->RemoveCollider(connectedEnemies_[i].get());
 				
-				connectedEnemies_[i].reset();  // 安全に削除
+				connectedEnemies_[i].reset();  // 安全に削
+				isChain_ = false;
 				removeCount_++;
 
 			} else{
@@ -70,13 +99,17 @@ void ChainEnemy::Update(){
 }
 
 void ChainEnemy::Draw(){
-	if (connectedEnemies_[0] && connectedEnemies_[1]){
-		Vector3 firstEnemyPos = connectedEnemies_[0]->GetCenterPos();
-		Vector3 secondEnemyPos = connectedEnemies_[1]->GetCenterPos();
 
-		//つながっている敵の間に線を引く
-		PrimitiveDrawer::GetInstance()->DrawLine3d(firstEnemyPos, secondEnemyPos, {0.0f, 0.0f, 0.0f, 1.0f});
+	if (isChain_){
+		if (connectedEnemies_[0] && connectedEnemies_[1]){
+			Vector3 firstEnemyPos = connectedEnemies_[0]->GetCenterPos();
+			Vector3 secondEnemyPos = connectedEnemies_[1]->GetCenterPos();
+
+			//つながっている敵の間に線を引く
+			PrimitiveDrawer::GetInstance()->DrawLine3d(firstEnemyPos, secondEnemyPos, {0.0f, 0.0f, 0.0f, 1.0f});
+		}
 	}
+	
 
 	for (const auto& enemy : connectedEnemies_){
 		if (enemy){
