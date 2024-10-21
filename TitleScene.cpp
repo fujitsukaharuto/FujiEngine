80d94716cc70c9a9085cb6c4019f01971d377c72
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-10-21 10:27:41 +0900

    title

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -15,6 +15,7 @@ TitleScene::TitleScene() {}
 
 TitleScene::~TitleScene() {
 	delete sphere;
+	curtain_.reset();
 }
 
 void TitleScene::Initialize() {
@@ -26,6 +27,12 @@ void TitleScene::Initialize() {
 
 	sphere = new Object3d();
 	sphere->CreateSphere();
+
+
+	curtain_.reset(new Sprite());
+	curtain_->Load("curtain.png");
+	curtain_->SetPos({ 640,360,0.0f });
+	curtain_->SetSize({ 1280.0f,720.0f });
 
 }
 
@@ -84,6 +91,8 @@ void TitleScene::Draw() {
 #pragma region 前景スプライト
 
 	dxCommon_->PreSpriteDraw();
+
+	curtain_->Draw();
 
 #pragma endregion
 
--- TitleScene.h
+++ TitleScene.h
@@ -21,4 +21,8 @@ private:
 	Object3d* sphere = nullptr;
 	Vector3 rightDir = { 1.0f,0.0f,0.0f };
 
+
+
+	std::unique_ptr<Sprite> curtain_;
+
 };
