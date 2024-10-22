ff3e2eb1e368673de6b66bbadff97d9565376b42
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-10-23 03:25:31 +0900

    teki

--- Enemy.h
+++ Enemy.h
@@ -1,1 +1,27 @@
 #pragma once
+#include "Object3d.h"
+
+
+class Enemy {
+public:
+	Enemy();
+	~Enemy();
+
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+private:
+
+
+
+private:
+
+
+	Object3d* enemy;
+
+};
