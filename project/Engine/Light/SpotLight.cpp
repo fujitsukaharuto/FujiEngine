03233d9bfe3bcaa6943297710c70f3a6923af28b
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-01-16 12:05:54 +0900

    light

--- project/Engine/Light/SpotLight.cpp
+++ project/Engine/Light/SpotLight.cpp
@@ -10,10 +10,10 @@ void SpotLight::Initialize() {
 	spotLightResource_->Map(0, nullptr, reinterpret_cast<void**>(&spotLightData_));
 	spotLightData_->color = { 1.0f,1.0f,1.0f,1.0f };
 	spotLightData_->position = { 0.0f,2.0f,0.0f };
-	spotLightData_->intensity = 1.0f;
-	spotLightData_->distance = 6.0f;
+	spotLightData_->intensity = 2.0f;
+	spotLightData_->distance = 40.0f;
 	spotLightData_->direction = Vector3({ 0.0f,-1.0f,0.0f }).Normalize();
-	spotLightData_->decay = 2.0f;
+	spotLightData_->decay = 0.15f;
 	spotLightData_->cosAngle = std::cos(std::numbers::pi_v<float> / 3.0f);
 	spotLightData_->cosFalloffStart = 3.0f;
 
