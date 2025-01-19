8cbcf735e56b1a177b5be98fbe9f357208f3429d
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-01-20 04:04:50 +0900

    scene

---
+++ project/Game/Scene/GameOverScene.cpp
@@ -0,0 +1,146 @@
+#include "GameOverScene.h"
+#include "ImGuiManager.h"
+#include "ModelManager.h"
+#include "GlobalVariables.h"
+#include "CameraManager.h"
+#include "FPSKeeper.h"
+#include "Random.h"
+
+#include "Particle/ParticleManager.h"
+#include "Scene/SceneManager.h"
+
+
+
+GameOverScene::GameOverScene() {}
+
+GameOverScene::~GameOverScene() {
+}
+
+void GameOverScene::Initialize() {
+	Init();
+
+
+	obj3dCommon.reset(new Object3dCommon());
+	obj3dCommon->Initialize();
+
+
+#pragma region シーン遷移用
+	black_ = std::make_unique<Sprite>();
+	black_->Load("white2x2.png");
+	black_->SetColor({ 0.0f,0.0f,0.0f,1.0f });
+	black_->SetSize({ 1280.0f,950.0f });
+	black_->SetAnchor({ 0.0f,0.0f });
+#pragma endregion
+
+	clearPaneru_ = std::make_unique<Sprite>();
+	clearPaneru_->Load("clear_none.png");
+	clearPaneru_->SetAnchor({ 0.0f,0.0f });
+
+	sphere = std::make_unique<Object3d>();
+	sphere->CreateSphere();
+	sphere->SetColor({ 1.0f,1.0f,0.0f,1.0f });
+
+}
+
+void GameOverScene::Update() {
+
+#ifdef _DEBUG
+
+
+	ImGui::Begin("Sphere");
+
+	ImGui::DragFloat3("scale", &sphere->transform.scale.x, 0.01f);
+	ImGui::DragFloat3("rotate", &sphere->transform.rotate.x, 0.01f);
+	ImGui::DragFloat3("right", &rightDir.x, 0.01f);
+	rightDir = rightDir.Normalize();
+	sphere->SetRightDir(rightDir);
+	ImGui::End();
+
+
+#endif // _DEBUG
+
+
+	dxCommon_->UpDate();
+
+	BlackFade();
+
+	sphere->transform.rotate.y += 0.02f;
+
+
+
+	ParticleManager::GetInstance()->Update();
+}
+
+void GameOverScene::Draw() {
+
+#pragma region 背景描画
+
+
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+
+#pragma region 3Dオブジェクト
+	obj3dCommon->PreDraw();
+	sphere->Draw();
+
+
+	ParticleManager::GetInstance()->Draw();
+
+#pragma endregion
+
+
+#pragma region 前景スプライト
+
+	dxCommon_->PreSpriteDraw();
+
+	clearPaneru_->Draw();
+	if (blackTime != 0.0f) {
+		black_->Draw();
+	}
+
+#pragma endregion
+
+}
+
+void GameOverScene::BlackFade() {
+	if (isChangeFase) {
+		if (blackTime < blackLimmite) {
+			blackTime += FPSKeeper::DeltaTime();
+			if (blackTime >= blackLimmite) {
+				blackTime = blackLimmite;
+			}
+		}
+		else {
+			SceneManager::GetInstance()->ChangeScene("TITLE", 40.0f);
+		}
+	}
+	else {
+		if (blackTime > 0.0f) {
+			blackTime -= FPSKeeper::DeltaTime();
+			if (blackTime <= 0.0f) {
+				blackTime = 0.0f;
+			}
+		}
+	}
+	black_->SetColor({ 0.0f,0.0f,0.0f,Lerp(0.0f,1.0f,(1.0f / blackLimmite * blackTime)) });
+	XINPUT_STATE pad;
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+		if (blackTime == 0.0f) {
+			isChangeFase = true;
+		}
+	}
+
+	else if (Input::GetInstance()->GetGamepadState(pad)) {
+		if (Input::GetInstance()->TriggerButton(PadInput::Y)) {
+			if (blackTime == 0.0f) {
+				isChangeFase = true;
+			}
+		}
+	}
+}
+
+void GameOverScene::ApplyGlobalVariables() {
+
+
+}
