a30b86fe37e5f445ba81befb08fc0f73bab77a2f
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-01-21 16:49:13 +0900

    imgui

--- project/Engine/ImGuiManager/ImGuiManager.cpp
+++ project/Engine/ImGuiManager/ImGuiManager.cpp
@@ -103,9 +103,13 @@ void ImGuiManager::Draw()
 }
 
 void ImGuiManager::SetFontJapanese() {
+#ifdef _DEBUG
 	ImGui::PushFont(font_japanese);
+#endif // _DEBUG
 }
 
 void ImGuiManager::UnSetFont() {
+#ifdef _DEBUG
 	ImGui::PopFont();
+#endif // _DEBUG
 }
