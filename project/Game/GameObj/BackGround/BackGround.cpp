146be9b0eda975ab66e6df7bc2dbdd158397d1a5
ku-rihara <k023g0027@g.neec.ac.jp>
2025-02-05 13:52:12 +0900

    背景クラス追加１

---
+++ project/Game/GameObj/BackGround/BackGround.cpp
@@ -0,0 +1,13 @@
+#include"BackGround.h"
+
+void BackGround::Init() {
+	sprite_ = std::make_unique<Sprite>();
+}
+
+void BackGround::Update() {
+
+}
+
+void BackGround::Draw() {
+
+}
--- project/Game/GameObj/BackGround/BackGround.h
+++ project/Game/GameObj/BackGround/BackGround.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include"Sprite.h"
+
 class BackGround {
 private:
-
+	std::unique_ptr<Sprite>sprite_
 
 public:
-
+	void Init();
+	void Update();
+	void Draw();
 };
