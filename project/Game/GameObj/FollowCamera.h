0f67043d8e30e178ec0717189c95a9c8f7f8fc7e
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-04-11 12:58:04 +0900

    fix

---
+++ project/Game/GameObj/FollowCamera.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Engine/Math/Matrix/MatrixCalculation.h"
+
+
+class FollowCamera {
+public:
+	FollowCamera();
+	~FollowCamera();
+
+public:
+
+	void Initialize();
+	void Update();
+
+	void SetTarget(const Trans* target);
+	void Reset();
+
+
+	Vector3 OffsetCal() const;
+
+private:
+
+
+private:
+
+	const Trans* target_ = nullptr;
+	Vector3 interTarget_ = {};
+	float destinationAngleY_ = 0;
+
+};
