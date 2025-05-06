5bd616e69126c54851ead60f1e2a851d103d66f8
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-05-06 16:50:00 +0900

    add boss

--- project/Game/GameObj/Enemy/Boss.h
+++ project/Game/GameObj/Enemy/Boss.h
@@ -2,6 +2,7 @@
 #include "Game/OriginGameObject.h"
 #include "Game/Collider/AABBCollider.h"
 
+#include "Game/GameObj/Enemy/Behavior/BaseBossBehavior.h"
 
 class Boss : public OriginGameObject {
 public:
@@ -13,8 +14,17 @@ public:
 	void Draw(Material* mate = nullptr)override;
 	void DebugGUI()override;
 
-
+	//========================================================================*/
+	//* Behavior
+	void ChangeBehavior(std::unique_ptr<BaseBossBehavior>behavior);
 
 private:
+
+private:
+
+	std::unique_ptr<BaseBossBehavior> behavior_ = nullptr;
+
+	std::unique_ptr<Object3d> shadow_;
+	std::unique_ptr<AABBCollider> collider_;
 
 };
