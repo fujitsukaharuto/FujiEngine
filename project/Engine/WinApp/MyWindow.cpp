1d96ce6ed30203a25a204a77e4398642e9b6b4af
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-06-03 10:17:22 +0900

    fix windowProcessMessage

--- project/Engine/WinApp/MyWindow.cpp
+++ project/Engine/WinApp/MyWindow.cpp
@@ -29,11 +29,11 @@ bool MyWin::ProcessMessage() {
 	MSG msg{};
 
 	while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+		if (msg.message == WM_QUIT) {
+			return true;
+		}
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
-	}
-	if (msg.message == WM_QUIT) {
-		return true;
 	}
 	return false;
 }
