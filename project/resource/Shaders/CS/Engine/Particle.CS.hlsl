5b3f0373cf583d41678296e4173998aadec58159
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2025-07-21 15:56:39 +0900

    CSParticle準備

--- project/resource/Shaders/CS/Engine/Particle.CS.hlsl
+++ project/resource/Shaders/CS/Engine/Particle.CS.hlsl
@@ -1,4 +1,21 @@
-[numthreads(1, 1, 1)]
+struct Particle
+{
+    float3 translate;
+    float3 scale;
+    float lifeTime;
+    float3 velocity;
+    float currentTime;
+    float4 color;
+};
+static const uint kMakParticles = 1024;
+RWStructuredBuffer<Particle> gParticle : register(u0);
+
+[numthreads(1024, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
+    uint particleIndex = DTid.x;
+    if (particleIndex < kMakParticles)
+    {
+        gParticle[particleIndex] = (Particle) 0;
+    }
 }
